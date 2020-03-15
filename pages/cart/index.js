35a0a797326e7f8cb2d244299f32546e6bd64194
1752265235 <1752265235@qq.com>
2020-03-15 21:15:43 +0800

    商品详情，购物车修改1

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -5,15 +5,65 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // cartFoodList: [],
+    // isCheckAll: true,
+    // totalSum: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // this.data.cartFoodList = JSON.parse(options.data)
 
+    // this.setData({
+    //   cartFoodList: this.data.cartFoodList
+    // })
+    // this.calcTotalSum();
   },
+
+  // choose(e) {
+  //   let index = e.currentTarget.dataset.idx;
+  //   this.data.cartFoodList[index].checked = !this.data.cartFoodList[index].checked;
+
+  //   let isCheckAll = this.data.cartFoodList.every(item => item.checked)
+
+  //   //let isCheckAll = this.data.cartFoodList.every(item => {
+  //   //  return item.checked == true
+  //   //})
+
+  //   this.setData({
+  //     cartFoodList: this.data.cartFoodList,
+  //     isCheckAll
+  //   })
+  //   this.calcTotalSum();
+  // },
+
+  // calcTotalSum() {
+  //   let totalSum = 0;
+  //   this.data.cartFoodList.forEach(item => {
+  //     if (item.checked) {
+  //       totalSum += item.price * item.count
+  //     }
+  //   })
+  //   this.setData({
+  //     totalSum
+  //   })
+  // },
+
+  // chooseAll() {
+  //   let isCheckAll = !this.data.isCheckAll;
+
+  //   this.data.cartFoodList.forEach(function (item) {
+  //     item.checked = isCheckAll;
+  //   })
+
+  //   this.setData({
+  //     isCheckAll,
+  //     cartFoodList: this.data.cartFoodList
+  //   })
+  //   this.calcTotalSum();
+  // },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
